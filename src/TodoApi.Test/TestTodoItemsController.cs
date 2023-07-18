a2fdaaa524094289c6e6249409b74a24fe02ae5b
Priscila <priscilasartori2@gmail.com>
2023-07-18 20:32:24 -0300

    feat: Corrija as rotas de buscar Tarefas

--- src/TodoApi.Test/TestTodoItemsController.cs
+++ src/TodoApi.Test/TestTodoItemsController.cs
@@ -9,69 +9,70 @@ namespace TodoApi.Test;
 
 public class TestTodoItemsController : IClassFixture<WebApplicationFactory<Program>>
 {
-    public HttpClient _client;
+  public HttpClient _client;
 
-    public TestTodoItemsController(WebApplicationFactory<Program> factory)
+  public TestTodoItemsController(WebApplicationFactory<Program> factory)
+  {
+    _client = factory.WithWebHostBuilder(builder =>
     {
-        _client = factory.WithWebHostBuilder(builder =>
-        {
-            builder.ConfigureServices(services =>
-            {
-                services.AddDbContext<TodoContext>(options =>
-                {
-                    options.UseInMemoryDatabase("TodoList");
-                });
-
-                var sp = services.BuildServiceProvider();
-                
-                var scope = sp.CreateScope();
-                var scopedServices = scope.ServiceProvider;
-                var db = scopedServices.GetRequiredService<TodoContext>();
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-                db.TodoItems.AddRange(GetTodoTestList());
-                db.SaveChanges();
+      builder.ConfigureServices(services =>
+          {
+          services.AddDbContext<TodoContext>(options =>
+              {
+              options.UseInMemoryDatabase("TodoList");
             });
-        }).CreateClient();
-    }
 
-    [Theory]
-    [InlineData("/api/TodoItems",HttpStatusCode.OK)]
-    [InlineData("/api/TodoItems/5",HttpStatusCode.NotFound)]
-    public async Task TestGetRoutesStatusCode(string path, HttpStatusCode expectedStatusCode)
-    {
-        throw new NotImplementedException();
-    }
+          var sp = services.BuildServiceProvider();
 
-    [Theory]
-    [InlineData("/api/TodoItems/1",HttpStatusCode.NoContent)]
-    [InlineData("/api/TodoItems/5",HttpStatusCode.NotFound)]
-    public async Task TestDeleteRouteStatusCode(string path, HttpStatusCode expectedStatusCode)
-    {
-        throw new NotImplementedException();
-    }
+          var scope = sp.CreateScope();
+          var scopedServices = scope.ServiceProvider;
+          var db = scopedServices.GetRequiredService<TodoContext>();
+          db.Database.EnsureDeleted();
+          db.Database.EnsureCreated();
+          db.TodoItems.AddRange(GetTodoTestList());
+          db.SaveChanges();
+        });
+    }).CreateClient();
+  }
+
+  [Theory]
+  [InlineData("/api/TodoItems", HttpStatusCode.OK)]
+  [InlineData("/api/TodoItems/5", HttpStatusCode.NotFound)]
+  public async Task TestGetRoutesStatusCode(string path, HttpStatusCode expectedStatusCode)
+  {
+    var response = await _client.GetAsync(path);
+    response.StatusCode.Should().Be(expectedStatusCode);
+  }
+
+  [Theory]
+  [InlineData("/api/TodoItems/1", HttpStatusCode.NoContent)]
+  [InlineData("/api/TodoItems/5", HttpStatusCode.NotFound)]
+  public async Task TestDeleteRouteStatusCode(string path, HttpStatusCode expectedStatusCode)
+  {
+    throw new NotImplementedException();
+  }
 
 
-    [Theory]
-    [InlineData("/api/TodoItems", "{\"name\": \"Minha Tarefa\"}", HttpStatusCode.Created)]
-    [InlineData("/api/TodoItems", "{\"teste\": \"testando\"}", HttpStatusCode.BadRequest)]
-    public async Task TestPostRouteStatusCode(string path, string jsonToAdd, HttpStatusCode expectedStatusCode)
-    {
-        throw new NotImplementedException();
-    }
+  [Theory]
+  [InlineData("/api/TodoItems", "{\"name\": \"Minha Tarefa\"}", HttpStatusCode.Created)]
+  [InlineData("/api/TodoItems", "{\"teste\": \"testando\"}", HttpStatusCode.BadRequest)]
+  public async Task TestPostRouteStatusCode(string path, string jsonToAdd, HttpStatusCode expectedStatusCode)
+  {
+    throw new NotImplementedException();
+  }
 
-    [Theory]
-    [InlineData("/api/TodoItems/1", "{\"id\": 1, \"name\": \"Minha Tarefa\"}", HttpStatusCode.OK)]
-    [InlineData("/api/TodoItems/200", "{\"id\": 200, \"name\": \"testando\"}", HttpStatusCode.NotFound)]
-    [InlineData("/api/TodoItems/5", "{\"test\": \"testando\"}", HttpStatusCode.BadRequest)]
-    public async Task TestPutRouteStatusCode(string path, string jsonToAdd, HttpStatusCode expectedStatusCode)
-    {
-        throw new NotImplementedException();
-    }
+  [Theory]
+  [InlineData("/api/TodoItems/1", "{\"id\": 1, \"name\": \"Minha Tarefa\"}", HttpStatusCode.OK)]
+  [InlineData("/api/TodoItems/200", "{\"id\": 200, \"name\": \"testando\"}", HttpStatusCode.NotFound)]
+  [InlineData("/api/TodoItems/5", "{\"test\": \"testando\"}", HttpStatusCode.BadRequest)]
+  public async Task TestPutRouteStatusCode(string path, string jsonToAdd, HttpStatusCode expectedStatusCode)
+  {
+    throw new NotImplementedException();
+  }
 
-    public static List<TodoItem> GetTodoTestList()
-    {
-        return new List<TodoItem>()
+  public static List<TodoItem> GetTodoTestList()
+  {
+    return new List<TodoItem>()
         {
             new TodoItem()
             {
@@ -92,5 +93,5 @@ public class TestTodoItemsController : IClassFixture<WebApplicationFactory<Progr
                 IsComplete = true
             },
         };
-    }
+  }
 }
